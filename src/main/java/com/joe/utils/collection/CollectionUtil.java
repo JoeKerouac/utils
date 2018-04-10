d0874fb9c3baa8430a8a5e7e92778efddaaf1f87
joe <1213812243@qq.com>
2018-04-10 17:32:54 +0800

    增加内连接操作

--- src/main/java/com/joe/utils/collection/CollectionUtil.java
+++ src/main/java/com/joe/utils/collection/CollectionUtil.java
@@ -3,6 +3,7 @@ package com.joe.utils.collection;
 import com.joe.utils.common.MathUtils;
 
 import java.util.*;
+import java.util.function.BiFunction;
 
 /**
  * 集合工具类
@@ -10,6 +11,83 @@ import java.util.*;
  * @author joe
  */
 public final class CollectionUtil {
+    /**
+     * 将多个数组内连接
+     *
+     * @param list     多个数组集合，不能为空
+     * @param function 连接函数，使用该函数连接，不能为空
+     * @param <T>      数组内数据类型
+     * @return 内连接结果，例如原数组为[[1,2],[3,4],[5,6]]，内连接结果为[135,136,145,146,235,236,245,246]
+     */
+    public static <T> List<T> innerJoin(List<List<T>> list, BiFunction<T, T, T> function) {
+        if (list == null || function == null) {
+            throw new NullPointerException("list or function must not be null");
+        }
+        if (list.isEmpty()) {
+            return Collections.emptyList();
+        }
+        if (list.size() == 1) {
+            return list.get(0);
+        }
+
+        List<List<T>> result = innerJoin(list.get(0), list.get(1), function);
+        if (list.size() == 2) {
+            return merge(result);
+        } else {
+            for (int i = 2; i < list.size(); i++) {
+                List<T> l1 = list.get(i);
+                List<List<T>> temp = new ArrayList<>();
+                result.stream().forEach(l -> temp.addAll(innerJoin(l, l1, function)));
+                result = temp;
+            }
+            return merge(result);
+        }
+    }
+
+    /**
+     * 合并数组，将多个数组中的内容合并到一个数组，例如参数数组为[[1,2],[3,4]]，合并后为[1,2,3,4]
+     *
+     * @param list 要合并的数组
+     * @param <T>  数据类型
+     * @return 合并结果
+     */
+    public static <T> List<T> merge(List<List<T>> list) {
+        List<T> result = new ArrayList<>();
+        list.stream().forEach(result::addAll);
+        return result;
+    }
+
+    /**
+     * 将两个数组内连接，原数组为[[1,2],[3,4]]，内连接后为[[13, 14], [23, 24]]
+     *
+     * @param l1       第一个数组
+     * @param l2       第二个数组
+     * @param function 连接函数
+     * @param <T>      数据类型
+     * @return 连接结果
+     */
+    public static <T> List<List<T>> innerJoin(List<T> l1, List<T> l2, BiFunction<T, T, T> function) {
+        if (l1 == null || l2 == null) {
+            throw new NullPointerException("inner join arrays must not be null");
+        }
+
+        if (l1.isEmpty() && l2.isEmpty()) {
+            return Collections.emptyList();
+        } else if (l1.isEmpty()) {
+            return Collections.singletonList(l2);
+        } else if (l2.isEmpty()) {
+            return Collections.singletonList(l1);
+        }
+
+        List<List<T>> result = new ArrayList<>(l1.size() * l2.size());
+        l1.stream().forEach(t1 -> {
+            List<T> l = new ArrayList<>();
+            l2.stream().forEach(t2 -> l.add(function.apply(t1, t2)));
+            result.add(l);
+        });
+        return result;
+    }
+
     /**
      * 将集合中的数据全排列
      *
