b0b8b40a9e31a0e5902556d0d012d2f13e738f12
JoeKerouac <1213812243@qq.com>
2018-06-04 17:50:53 +0800

    增加PageData

---
+++ src/main/java/com/joe/utils/data/PageData.java
@@ -0,0 +1,38 @@
+package com.joe.utils.data;
+
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * @author joe
+ * @version 2018.06.04 16:52
+ */
+@Data
+public class PageData<T> {
+    /**
+     * 数据
+     */
+    private List<T> datas;
+    /**
+     * 当前页数，从0开始
+     */
+    private int currentPage;
+    /**
+     * 每页最多显示多少数据
+     */
+    private int limit;
+    /**
+     * 总数据量
+     */
+    private int total;
+    /**
+     * 总页数
+     */
+    private int totalPage;
+    /**
+     * 是否有下一页，true表示有下一页
+     */
+    private boolean hasNext;
+
+}
