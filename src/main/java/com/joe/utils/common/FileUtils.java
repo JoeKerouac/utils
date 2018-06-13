3e560fcd6a606a5469cd9e0832aa0b6df449914e
JoeKerouac <1213812243@qq.com>
2018-06-13 18:24:03 +0800

    修改文件遍历工具BUG

--- src/main/java/com/joe/utils/common/FileUtils.java
+++ src/main/java/com/joe/utils/common/FileUtils.java
@@ -14,11 +14,15 @@ public class FileUtils {
      * 遍历指定文件夹下所有文件
      *
      * @param file 文件夹
-     * @return 指定文件夹下所有文件（不包括本文件夹，不包括里边所有文件夹，只有文件）
+     * @return 指定文件夹下所有文件（不包括文件夹，只有文件，如果给定的不是文件夹而是文件的话将会返回文件本身）
      */
     public static List<File> findAllFile(File file) {
-        if (file == null || !file.exists() || file.isFile()) {
+        if (file == null || !file.exists()) {
             return Collections.emptyList();
+        }
+
+        if (file.isFile()) {
+            return Collections.singletonList(file);
         }
 
         List<File> fileList = new ArrayList<>();
