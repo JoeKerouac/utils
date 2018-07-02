f39933a5d4bb966f962ad5e87e292b4758661ca6
JoeKerouac <1213812243@qq.com>
2018-07-02 15:35:26 +0800

    增加bean验证器

---
+++ src/main/java/com/joe/utils/validator/ValidationException.java
@@ -0,0 +1,28 @@
+package com.joe.utils.validator;
+
+/**
+ * @author joe
+ * @version 2018.07.02 15:23
+ */
+public class ValidationException extends RuntimeException {
+    public ValidationException() {
+        super();
+    }
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(Throwable cause) {
+        super(cause);
+    }
+
+    protected ValidationException(String message, Throwable cause, boolean enableSuppression, boolean
+            writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/com/joe/utils/validator/ValidatorUtil.java
@@ -0,0 +1,66 @@
+package com.joe.utils.validator;
+
+
+import javax.validation.*;
+import javax.validation.executable.ExecutableValidator;
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * bean校验工具，不符合规则的会抛出异常
+ *
+ * @author joe
+ * @version 2018.07.02 15:15
+ */
+public class ValidatorUtil {
+    /**
+     * bean验证器
+     */
+    private static final Validator validator;
+    /**
+     * 构造器、方法参数、方法响应验证器
+     */
+    private static final ExecutableValidator executableValidator;
+
+    static {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+        executableValidator = validator.forExecutables();
+    }
+
+    /**
+     * 校验bean是否符合规则
+     *
+     * @param bean 要校验的bean
+     */
+    public static void validate(Object bean) {
+        Set<ConstraintViolation<Object>> constraintViolations = validator.validate(bean);
+        check(constraintViolations);
+    }
+
+    /**
+     * 验证方法参数是否符合规则
+     *
+     * @param instance 方法所在的类的实例
+     * @param method   方法实例
+     * @param params   参数
+     */
+    public static void validateParameters(Object instance, Method method, Object[] params) {
+        Set<ConstraintViolation<Object>> constraintViolations = executableValidator.validateParameters(instance,
+                method, params);
+        check(constraintViolations);
+    }
+
+    /**
+     * 检查是否有校验错误
+     *
+     * @param constraintViolations 校验结果
+     */
+    private static void check(Set<ConstraintViolation<Object>> constraintViolations) {
+        Iterator<ConstraintViolation<Object>> iterator = constraintViolations.iterator();
+        if (iterator.hasNext()) {
+            throw new ValidationException(iterator.next().getMessage());
+        }
+    }
+}
