d78abdbb1b71ffbd4a2090026b3579e8b4e4a6b8
JoeKerouac <1213812243@qq.com>
2018-07-25 15:43:34 +0800

    修复数字正则

--- src/main/java/com/joe/utils/pattern/PatternUtils.java
+++ src/main/java/com/joe/utils/pattern/PatternUtils.java
@@ -15,16 +15,20 @@ public class PatternUtils {
     /**
      * IP正则
      */
-    private static final Pattern IP_PATTERN    = Pattern
+    private static final Pattern IP_PATTERN        = Pattern
         .compile("[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}");
     /**
      * 手机号正则
      */
-    private static final Pattern PHONE_PATTERN = Pattern.compile("^1[3|4|5|7|8]\\d{9}$");
+    private static final Pattern PHONE_PATTERN     = Pattern.compile("^1[3|4|5|7|8]\\d{9}$");
     /**
      * 匹配数字正则（开头不能是0，末尾可以是0）
      */
-    private static Pattern       NUMBER_PARTER = Pattern.compile("-?(((1-9)\\d*)|(\\d))(\\.\\d*)");
+    private static Pattern       NUMBER_PARTER     = Pattern.compile("-?[1-9]+\\d*(\\.\\d+)");
+    /**
+     * 匹配数字字符串，只要字符串包含的只有数字即可
+     */
+    private static Pattern       NUMBER_STR_PARTER = Pattern.compile("[0-9]+");
 
     /**
      * 判断字符串是否是IP格式的字符串
@@ -37,17 +41,29 @@ public class PatternUtils {
     }
 
     /**
-     * 判断字符串参数是否是数字
+     * 判断字符串参数是否是数字字符串，只要全是数字即可
      *
-     * @param arg 数字参数
+     * @param arg 字符串
+     * @return 如果参数是数字则返回<code>true</code>
+     */
+    public static boolean isNumberStr(String arg) {
+        if (StringUtils.isEmpty(arg)) {
+            return false;
+        }
+        return check(arg, NUMBER_STR_PARTER);
+    }
+
+    /**
+     * 判断字符串参数是否是数字（开头不能是0，结尾可以是0，可以包含小数点）
+     *
+     * @param arg 字符串
      * @return 如果参数是数字则返回<code>true</code>
      */
     public static boolean isNumber(String arg) {
         if (StringUtils.isEmpty(arg)) {
             return false;
         }
-        Matcher matcher = NUMBER_PARTER.matcher(arg);
-        return matcher.matches();
+        return check(arg, NUMBER_PARTER);
     }
 
     /**
