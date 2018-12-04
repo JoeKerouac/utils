946a8f7554bc9c26aa9eebfbc420d640b31fb10d
JoeKerouac <1213812243@qq.com>
2018-12-04 16:10:12 +0800

    增加十六进制的反解析方法；

--- src/main/java/com/joe/utils/codec/Hex.java
+++ src/main/java/com/joe/utils/codec/Hex.java
@@ -18,6 +18,48 @@ public class Hex {
                                                  'A', 'B', 'C', 'D', 'E', 'F' };
 
     /**
+     * 将十六进制字符串解析为byte数组
+     * @param hexStr 16进制字符串
+     * @return byte数组
+     */
+    public static byte[] decodeHex(final String hexStr) {
+        return decodeHex(hexStr.getBytes());
+    }
+
+    /**
+     * 将十六进制字符解析为byte数组
+     * @param hexDatas 16进制字符
+     * @return byte数组
+     */
+    public static byte[] decodeHex(final char[] hexDatas) {
+        if (hexDatas.length % 2 != 0) {
+            throw new IllegalArgumentException("非法的16进制字符");
+        }
+        byte[] data = new byte[hexDatas.length >> 1];
+        for (int i = 0; i < hexDatas.length; i += 2) {
+            data[i >> 1] = (byte) ((indexAt((byte) hexDatas[i]) << 4)
+                                   | indexAt((byte) hexDatas[i + 1]));
+        }
+        return data;
+    }
+
+    /**
+     * 将十六进制形式字符解析为byte数组
+     * @param hexDatas 16进制字符byte形式
+     * @return byte数组
+     */
+    public static byte[] decodeHex(final byte[] hexDatas) {
+        if (hexDatas.length % 2 != 0) {
+            throw new IllegalArgumentException("非法的16进制字符");
+        }
+        byte[] data = new byte[hexDatas.length >> 1];
+        for (int i = 0; i < hexDatas.length; i += 2) {
+            data[i >> 1] = (byte) ((indexAt(hexDatas[i]) << 4) | indexAt(hexDatas[i + 1]));
+        }
+        return data;
+    }
+
+    /**
      * 将byte数组转换为16进制字符数组
      *
      * @param data    数据
@@ -44,5 +86,21 @@ public class Hex {
             out[j++] = toDigits[0x0F & data[i]];
         }
         return out;
+    }
+
+    /**
+     * 获取16进制的char在数组{@link #DIGITS_LOWER}或者{@link #DIGITS_UPPER}中的位置
+     * @param c char
+     * @return 数组中的位置
+     */
+    private static int indexAt(byte c) {
+        //0-9对应的char范围是48-57；A-Z对应的char范围是65-90；a-z对应的char范围是97-122
+        if (c < 58) {
+            return (c - 48);
+        } else if (c < 91) {
+            return (c - 55);
+        } else {
+            return (c - 87);
+        }
     }
 }
--- src/test/java/com/joe/utils/codec/CodecTest.java
+++ src/test/java/com/joe/utils/codec/CodecTest.java
@@ -2,8 +2,9 @@ package com.joe.utils.codec;
 
 import java.util.Arrays;
 
+import com.joe.utils.common.StringUtils;
+import com.joe.utils.reflect.ReflectUtil;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -11,26 +12,31 @@ import org.junit.Test;
  * @version 2018.08.09 14:29
  */
 public class CodecTest {
-    byte[] datas;
-    String hex;
+    static byte[] datas;
+    static String hex;
+    static {
+        datas = new byte[] { 12, 56, 123, 1, 0, -45, 36, -123, -48, 94, 78, 53, 48, 12, 75, 48, 11,
+                             10, 34, 56, 91, 61, 43, 81, 61 };
+        hex = "0c387b0100d32485d05e4e35300c4b300b0a22385b3d2b513d";
+    }
 
     @Test
-    public void doHex() {
+    public void doHexEncodeTest() {
         Assert.assertEquals(new String(Hex.encodeHex(datas, true)), hex);
     }
 
     @Test
-    public void doBase64() {
+    public void doHexDecodeTest() {
+        Assert.assertTrue(Arrays.equals(datas, Hex.decodeHex(hex)));
+        Assert.assertTrue(
+            Arrays.equals(datas, Hex.decodeHex((char[]) ReflectUtil.getFieldValue(hex, "value"))));
+    }
+
+    @Test
+    public void doBase64Test() {
         String text = "测试文本";
         Assert.assertEquals(IBase64.decrypt(IBase64.encrypt(text)), text);
         Assert.assertTrue(
             Arrays.equals(text.getBytes(), IBase64.decrypt(IBase64.encrypt(text.getBytes()))));
-    }
-
-    @Before
-    public void init() {
-        datas = new byte[] { 12, 56, 123, 1, 0, -45, 36, -123, -48, 94, 78, 53, 48, 12, 75, 48, 11,
-                             10, 34, 56, 91, 61, 43, 81, 61 };
-        hex = "0c387b0100d32485d05e4e35300c4b300b0a22385b3d2b513d";
     }
 }
